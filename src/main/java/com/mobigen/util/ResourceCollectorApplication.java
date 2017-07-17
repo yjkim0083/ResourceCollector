e6149e9bef4250ef3eb0d611b4771f2ecd13b50e
yjkim0083 <yjkim0083@gmail.com>
2017-07-17 21:42:42 +0900

    commandlineRunner에서 schedule방식으로 변경

--- src/main/java/com/mobigen/util/ResourceCollectorApplication.java
+++ src/main/java/com/mobigen/util/ResourceCollectorApplication.java
@@ -1,26 +1,39 @@
 package com.mobigen.util;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 import com.mobigen.util.service.ResourceCollectService;
 
 @SpringBootApplication
-public class ResourceCollectorApplication implements CommandLineRunner {
+@EnableScheduling
+//public class ResourceCollectorApplication implements CommandLineRunner {
+public class ResourceCollectorApplication {
 
 	@Autowired
 	private ResourceCollectService service;
 
 	public static void main(String[] args) {
-		SpringApplication app = new SpringApplication(ResourceCollectorApplication.class);
-		app.run(args);
+//		SpringApplication app = new SpringApplication(ResourceCollectorApplication.class);
+//		app.run(args);
+		SpringApplication.run(ResourceCollectorApplication.class);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
+//	@Override
+//	public void run(String... args) throws Exception {
+////		service.getResource();
+////		System.exit(0);
+//	}
+	
+	@Scheduled(fixedRate = 1000 * 60)
+	public void scheduleTask() throws Exception {
 		service.getResource();
-		System.exit(0);
 	}
 }
