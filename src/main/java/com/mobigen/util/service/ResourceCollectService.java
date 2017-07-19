8c9324a7e34f550db561a7333cf3a602fa722c1f
yjkim0083 <yjkim0083@gmail.com>
2017-07-19 11:34:45 +0900

    gitignore 적용

--- src/main/java/com/mobigen/util/service/ResourceCollectService.java
+++ src/main/java/com/mobigen/util/service/ResourceCollectService.java
@@ -18,9 +18,11 @@ import oshi.hardware.CentralProcessor;
 import oshi.hardware.GlobalMemory;
 import oshi.hardware.HardwareAbstractionLayer;
 import oshi.hardware.NetworkIF;
+import oshi.hardware.CentralProcessor.TickType;
 import oshi.software.os.FileSystem;
 import oshi.software.os.OSFileStore;
 import oshi.software.os.OperatingSystem;
+import oshi.util.Util;
 
 @Service
 public class ResourceCollectService {
@@ -50,9 +52,14 @@ public class ResourceCollectService {
 	
 	private Map<String, String> getSystemResource() {
 		Map<String, String> resourceMap = new HashMap<String, String>();
-		resourceMap.put("cpu_usage", "");
-		resourceMap.put("ram_usage", "");
-		resourceMap.put("disk_usage", "");
+		resourceMap.put("cpu_usage", "0");
+		resourceMap.put("ram_usage", "0");
+		resourceMap.put("disk_usage", "0");
+		resourceMap.put("network_receive", "0");
+		resourceMap.put("network_send", "0");
+		resourceMap.put("iowait", "0");
+		resourceMap.put("load_avg", "0");
+		resourceMap.put("ip", serverIp);
 		resourceMap.put("datetime", new SimpleDateFormat("yyyyMMddHHmm00").format(new Date()));
 		
 		SystemInfo si = new SystemInfo();
@@ -64,14 +71,16 @@ public class ResourceCollectService {
         int cpuUsage = printCpu(hal.getProcessor());
         int diskUsage = printFileSystem(os.getFileSystem());
         long[] networkUsage = printNetworkInterfaces(hal.getNetworkIFs());
+        double[] cpuStatus = printCpuStatus(hal.getProcessor());
         
         resourceMap.put("cpu_usage", String.valueOf(cpuUsage));
         resourceMap.put("ram_usage", String.valueOf(memoryUsage));
 		resourceMap.put("disk_usage", String.valueOf(diskUsage));
 		resourceMap.put("network_receive", String.valueOf(networkUsage[0]));
 		resourceMap.put("network_send", String.valueOf(networkUsage[1]));
-		resourceMap.put("ip", serverIp);
-		
+		resourceMap.put("iowait", String.valueOf(cpuStatus[0]));
+		resourceMap.put("load_avg", String.valueOf(cpuStatus[1]));
+				
 		return resourceMap;
 	}
 
@@ -92,6 +101,33 @@ public class ResourceCollectService {
     private int printCpu(CentralProcessor processor) {
     	float usedCpu = (float)(processor.getSystemCpuLoadBetweenTicks() * 100);
     	return Math.round(usedCpu);
+    }
+    
+    private double[] printCpuStatus(CentralProcessor processor) {
+    	
+    	double[] result = new double[2];
+    	
+    	long[] prevTicks = processor.getSystemCpuLoadTicks();
+    	// Wait a second...
+        Util.sleep(1000);
+    	long[] ticks = processor.getSystemCpuLoadTicks();
+    	
+    	long user = ticks[TickType.USER.getIndex()] - prevTicks[TickType.USER.getIndex()];
+        long nice = ticks[TickType.NICE.getIndex()] - prevTicks[TickType.NICE.getIndex()];
+        long sys = ticks[TickType.SYSTEM.getIndex()] - prevTicks[TickType.SYSTEM.getIndex()];
+        long idle = ticks[TickType.IDLE.getIndex()] - prevTicks[TickType.IDLE.getIndex()];
+        long iowait = ticks[TickType.IOWAIT.getIndex()] - prevTicks[TickType.IOWAIT.getIndex()];
+        long irq = ticks[TickType.IRQ.getIndex()] - prevTicks[TickType.IRQ.getIndex()];
+        long softirq = ticks[TickType.SOFTIRQ.getIndex()] - prevTicks[TickType.SOFTIRQ.getIndex()];
+        long steal = ticks[TickType.STEAL.getIndex()] - prevTicks[TickType.STEAL.getIndex()];
+        long totalCpu = user + nice + sys + idle + iowait + irq + softirq + steal;
+        
+        result[0] = 100d * iowait / totalCpu;
+        
+        double[] loadAverage = processor.getSystemLoadAverage(3);
+        result[1] = loadAverage[0] < 0 ? 0.0 : loadAverage[0];
+        
+        return result;
     }
 
     /**
